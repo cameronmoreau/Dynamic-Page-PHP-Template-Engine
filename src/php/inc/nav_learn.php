590a7d4e48886bfe884b32f4045144a78caec97d
Cameron Moreau <cameron.cm6@gmail.com>
2014-02-03 23:03:07 -0600

    Create nav_learn.php

---
+++ src/php/inc/nav_learn.php
@@ -0,0 +1,17 @@
+<aside class="content_left">
+	<nav id="sub_nav">
+		<ul>
+			<li><a href="#">Learn</a></li>
+			<li><a href="content.php?page=learn_internet">The Internet</a></li>
+			<li><a href="#">HTML</a></li>
+			<li><a href="#">Getting Started</a></li>
+			<li><a href="#">Basic Structure</a></li>
+			<li><a href="#">Content</a></li>
+			<li><a href="#">Images</a></li>
+			<li><a href="#">Lists</a></li>
+			<li><a href="#">Tables</a></li>
+			<li><a href="#">Styling</a></li>
+			<li><a href="#">Compatibility</a></li>
+		</ul>
+	</nav>
+</aside>
